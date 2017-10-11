6ba603634f0d2f877a98693d65d96b43885e3e83
trivvet <trivvet@gmail.com>
2017-10-11 12:27:11 +0300

    End Stage 1

--- freports/views.py
+++ freports/views.py
@@ -9,49 +9,30 @@ from django.contrib import messages
 from .models import ForensicReport
 
 def reports_list(request):
-    reports = ForensicReport.objects.all()
+    reports = ForensicReport.objects.all().order_by('number')
+    order_by = request.GET.get('order_by')
+    if order_by:
+        reports = reports.order_by(order_by)
     return render(request, 'freports/reports_list.html', {'reports': reports})
 
 def add_report(request):
-    header = 'Додавання провадження'
+    header = u'Додавання провадження'
 
     if request.method == 'POST':
-        errors = {}
         if request.POST.get('save_button'):
-
-            report_number = request.POST.get('number')
-            if not report_number:
-                errors['number'] = '1'
-
-            address = request.POST.get('address')
-            if not address:
-                errors['address'] = '1'
-
-            plaintiff = request.POST.get('plaintiff')
-            if not plaintiff:
-                errors['plaintiff'] = '1'
-
-            defendant = request.POST.get('defendant')
-            if not defendant:
-                errors['defendant'] = '1'
-
-            object_name = request.POST.get('object_name')
-            if not object_name:
-                errors['object_name'] = '1'
-
-            research_kind = request.POST.get('research_kind')
-            if not research_kind:
-                errors['research_kind'] = '1'
+            validate_data = valid_report(request.POST)
+            errors = validate_data['errors']
+            new_report = validate_data['data_report']
 
             if errors:
                 messages.error(request, "Виправте наступні недоліки")
-                return render(request, 'freports/add_report.html', {'header': header})
+                return render(request, 'freports/add_report.html', {'header': header, 'errors': errors,
+                    'content': new_report})
 
             else:
-                new_report = ForensicReport(number=report_number, address=address, plaintiff=plaintiff, defendant=defendant,
-                    object_name=object_name, research_kind=research_kind)
+                new_report = ForensicReport(**new_report)
                 new_report.save()
-                messages.success(request, "Провадження №%s успішно додане" % report_number)
+                messages.success(request, "Провадження №%s/017 успішно додане" % new_report.number)
 
         elif request.POST.get('cancel_button'):
             messages.warning(request, "Додавання провадження скасовано")
@@ -67,48 +48,27 @@ def edit_report(request, rid):
     content = ForensicReport.objects.get(pk=rid)
 
     if request.method == 'POST':
-        errors = {}
         if request.POST.get('save_button'):
-
-            report_number = request.POST.get('number')
-            if not report_number:
-                errors['number'] = '1'
-
-            address = request.POST.get('address')
-            if not address:
-                errors['address'] = '1'
-
-            plaintiff = request.POST.get('plaintiff')
-            if not plaintiff:
-                errors['plaintiff'] = '1'
-
-            defendant = request.POST.get('defendant')
-            if not defendant:
-                errors['defendant'] = '1'
-
-            object_name = request.POST.get('object_name')
-            if not object_name:
-                errors['object_name'] = '1'
-
-            research_kind = request.POST.get('research_kind')
-            if not research_kind:
-                errors['research_kind'] = '1'
-
             current_report = content
-            current_report.number=report_number
-            current_report.address=address
-            current_report.plaintiff=plaintiff
-            current_report.defendant=defendant
-            current_report.object_name=object_name
-            current_report.research_kind=research_kind
+
+            validate_data = valid_report(request.POST)
+            errors = validate_data['errors']
+            new_report = validate_data['data_report']
 
             if errors:
-                messages.success(request, "Виправте наступні недоліки")
-                return render(request, 'freports/add_report.html', {'header': header, 'content': content})
+                messages.error(request, "Виправте наступні недоліки")
+                return render(request, 'freports/add_report.html', {'header': header, 'content': new_report,
+                    'errors': errors})
 
             else:
+                current_report.number = new_report['number']
+                current_report.address = new_report['address']
+                current_report.plaintiff = new_report['plaintiff']
+                current_report.defendant = new_report['defendant']
+                current_report.object_name = new_report['object_name']
+                current_report.research_kind = new_report['research_kind']
                 current_report.save()
-                messages.success(request, "Провадження №%s успішно відкориговане" % report_number)
+                messages.success(request, "Провадження №%s/017 успішно відкориговане" % current_report.number)
 
         elif request.POST.get('cancel_button'):
             messages.warning(request, "Редагування провадження скасовано")
@@ -132,5 +92,51 @@ def delete_report(request, rid):
             messages.warning(request, "Видалення провадження скасовано")
 
         return HttpResponseRedirect(reverse('forensic_reports_list'))
+
+def valid_report(data_post):
+    errors = {}
+    new_report = {}
+
+    report_number = data_post.get('number')
+    if not report_number:
+        errors['number'] = u"Номер висновку є обов'язковим"
+    else:
+        try:
+            new_report['number'] = int(report_number)
+        except ValueError:
+            new_report['number'] = report_number
+            errors['number'] = u"Будь-ласка введіть число"
+
+    address = data_post.get('address')
+    if not address:
+        errors['address'] = u"Адреса об'єкту є обов'язковою"
+    else:
+        new_report['address'] = address
+
+    plaintiff = data_post.get('plaintiff')
+    if not plaintiff:
+        errors['plaintiff'] = u"Прізвище позивача є обов'язковим"
+    else:
+        new_report['plaintiff'] = plaintiff
+
+    defendant = data_post.get('defendant')
+    if not defendant:
+        errors['defendant'] = u"Прізвище відповідача є обов'язковим"
+    else:
+        new_report['defendant'] = defendant
+
+    object_name = data_post.get('object_name')
+    if not object_name:
+        errors['object_name'] = u"Тип об'єкту є обов'язковим"
+    else:
+        new_report['object_name'] = object_name
+
+    research_kind = data_post.get('research_kind')
+    if not research_kind:
+        errors['research_kind'] = u"Вид дослідження є обов'язковим"
+    else:
+        new_report['research_kind'] = research_kind
+
+    return {'errors': errors, 'data_report': new_report}
 
 
